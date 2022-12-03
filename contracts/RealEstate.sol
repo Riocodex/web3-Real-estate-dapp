631250b51418bc2f193159ff6bd54c993d81eeaa
Rio <onwukachibike@gmail.com>
2022-12-03 13:28:03 +0100

    created the constructor function

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -7,5 +7,18 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 contract RealEstate is ERC721URIStorage {
     
     constructor() ERC721("Real Estate", "REAL") {}
-    
+
+    function mint(string memory tokenURI) public returns (uint256) {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _tokenIds.current();
+    }
 }
