8af7e6f67d7b587cbcc7a746e79e757623b2167a
Rio <onwukachibike@gmail.com>
2022-12-03 13:15:35 +0100

    updated

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -1,6 +1,11 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-contract RealEstate {
-
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+contract RealEstate is ERC721URIStorage {
+    
+    constructor() ERC721("Real Estate", "REAL") {}
+    
 }
