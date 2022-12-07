105fb95aa3c2721010273c0dda58cf7dbcd98c7b
Rio <onwukachibike@gmail.com>
2022-12-07 09:56:19 +0100

    updated the test to check updated variables

--- test/Escrow.js
+++ test/Escrow.js
@@ -1,6 +1,7 @@
 const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
+//tokens below converts tokens to ethereum..so tokens(10)means 10 eth
 const tokens = (n) => {
     return ethers.utils.parseUnits(n.toString(), 'ether')
 }
@@ -33,7 +34,7 @@ describe('Escrow', () => {
         await transaction.wait()
 
         //list property
-        transaction = await escrow.connect(seller).list(1)
+        transaction = await escrow.connect(seller).list(1, buyer.address, tokens(10), tokens(5))
         await transaction.wait()
     })
 
@@ -65,7 +66,23 @@ describe('Escrow', () => {
         it('Updates ownership', async () =>{
             expect(await realEstate.ownerOf(1)).to.be.equal(escrow.address)
         })
-   
+        
+        it('Returns Buyer', async()=>{
+            const result = await escrow.buyer(1);
+            expect(result).to.be.equal(buyer.address)
+        })
+        
+        it('Returns purchase Price', async()=>{
+            const result = await escrow.purchasePrice(1)
+            expect(result).to.be.equal(tokens(10))
+        })
+
+        it('Returns escrow amount', async ()=>{
+            const result = await escrow.escrowAmount(1)
+            expect(result).to.be.equal(tokens(5))
+        })
+
+        
     })
 
 })
