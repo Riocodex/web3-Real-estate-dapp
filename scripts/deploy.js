61d4dcb9a635fc5d395b28b5e974cc15cb25e779
Rio <onwukachibike@gmail.com>
2022-12-07 13:18:00 +0100

    deploying contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,7 +7,39 @@
 const hre = require("hardhat");
 
 async function main() {
+  //setup accounts
+  [buyer, seller, inspector, lender] = await ethers.getSigners()
 
+  //deploy real estate
+  const RealEstate = await ethers.getContractFactory('RealEstate')
+  const realEstate = await RealEstate.deploy()
+  await realEstate.deployed()
+
+  console.log('Deployed Real Estate Contract at: ', realEstate.address)
+  console.log("Minting properties.....\n")
+
+//Minting
+
+  for(let i=0; i<3; i++){
+    const transaction = await realEstate.connect(seller).mint(`https://ipfs.io/ipfs/QmQVcpsjrA6cr1iJjZAodYwmPekYgbnXGo4DFubJiLc2EB/${i+1}.json`)
+    await transaction.wait()
+  }
+}
+
+//deploy escrow
+const Escrow = await ethers.getContractFactory('Escrow')
+const escrow = await Escrow.deploy(
+  realEstate.address,
+  seller.address,
+  inspector.address,
+  lender.address
+)
+await escrow.deployed()
+
+for(let i=0; i<3; i++){
+  //Approve properties
+  let transaction = await realEstate.connect(seller).approve(escrow.address, i+1);
+  await transaction.wait()
 }
 
 // We recommend this pattern to be able to use async/await everywhere
