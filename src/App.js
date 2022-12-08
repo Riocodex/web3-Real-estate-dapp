e339200b024dd814e6e0cb46f385eb158261ae7c
Rio <onwukachibike@gmail.com>
2022-12-08 04:47:12 +0100

    updated front end

--- src/App.js
+++ src/App.js
@@ -21,6 +21,9 @@ function App() {
     const [account, setAccount ] = useState(null)
     const [homes, setHomes] = useState([])
 
+    const [home, setHome] = useState({})
+    const [toggle, setToggle] = useState(false) 
+
     const loadBlockchainData = async() => {
       const provider = new ethers.providers.Web3Provider(window.ethereum)
       setProvider(provider)
@@ -32,7 +35,7 @@ function App() {
             new ethers.Contract( config[network.chainId].realEstate.address, RealEstate, provider)
       const totalSupply = await realEstate.totalSupply()
       const homes = []
-      
+
       //getting the metadata from the nft
       for (var i = 1; i <= totalSupply; i++){
         const uri = await realEstate.tokenURI(i)
@@ -41,6 +44,7 @@ function App() {
         homes.push(metadata)
       }
       setHomes(homes)
+      console.log(homes)
       
       const escrow = 
             new ethers.Contract(config[network.chainId].escrow.address, Escrow, provider)
@@ -60,33 +64,50 @@ function App() {
     useEffect(() => {
       loadBlockchainData()
     }, [])
+
+    const toggleProp = (home) => {
+      setHome(home)
+      toggle ? setToggle(false) : setToggle(true)
+    }
   return (
     <div>
       <Navigation account={account} setAccount={setAccount}/> 
       <Search/>
       <div className='cards__section'>
 
-        <h3>Homes for you</h3>
+        <h3>Homes For You</h3>
         <hr/>
 
         <div className='cards'>
-          <div className='card'>
-            <div className='card__image'>
-              <img src="" alt="Home" />
-            </div>
-            <div className='card__info'>
-              <h4>1 ETH</h4>
-              <p>
-                <strong>1</strong> bds |
-                <strong>2</strong> ba  |
-                <strong>3</strong> sqft
-              </p>
-              <p>1234 Elm St</p>
-            </div>
-          </div>
+          {homes.map((home, index) => (
+
+                  <div className='card' key={index} onClick={() => toggleProp(home)}>
+                  <div className='card__image'>
+                    <img src={home.image} alt="Home" />
+                  </div>
+                  <div className='card__info'>
+                    <h4>{home.attributes[0].value} ETH</h4>
+                    <p>
+                      <strong>{home.attributes[2].value}</strong> bds |
+                      <strong>{home.attributes[3].value}</strong> ba  |
+                      <strong>{home.attributes[4].value}</strong> sqft
+                    </p>
+                    <p>{home.address}</p>
+                  </div>
+                </div>
+          ))}
+      
         </div>
 
       </div>
+      {toggle  && (
+        <Home 
+        home={home} 
+        provider={provider} 
+        account={account} 
+        escrow={escrow} 
+        toggleProp={toggleProp} />
+      )}
 
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,19 @@ import { useEffect, useState } from 'react';
 
 import close from '../assets/close.svg';
 
-const Home = ({ home, provider, escrow, togglePop }) => {
+const Home = ({ home, provider, escrow, toggleProp }) => {
 
     return (
         <div className="home">
+            <div className='home__details'>
+                <div className='home__image'>
+                    <img src={home.image} alt="Home" />
+                </div>
+                <button onClick={toggleProp} className="home__close">
+                    <img src={close} alt='close'/>
+                </button>
+            </div>
+            
         </div>
     );
 }
