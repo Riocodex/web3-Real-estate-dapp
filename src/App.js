80d0cd5a1c6225a891699ce7f2f0cca0848437b5
Rio <onwukachibike@gmail.com>
2022-12-08 04:25:10 +0100

    connected the nfts to the front end

--- src/App.js
+++ src/App.js
@@ -17,7 +17,9 @@ function App() {
 
     const [provider, setProvider] = useState(null)
     const [escrow, setEscrow] = useState(null)
+
     const [account, setAccount ] = useState(null)
+    const [homes, setHomes] = useState([])
 
     const loadBlockchainData = async() => {
       const provider = new ethers.providers.Web3Provider(window.ethereum)
@@ -30,6 +32,15 @@ function App() {
             new ethers.Contract( config[network.chainId].realEstate.address, RealEstate, provider)
       const totalSupply = await realEstate.totalSupply()
       const homes = []
+      
+      //getting the metadata from the nft
+      for (var i = 1; i <= totalSupply; i++){
+        const uri = await realEstate.tokenURI(i)
+        const response = await fetch(uri)
+        const metadata = await response.json()
+        homes.push(metadata)
+      }
+      setHomes(homes)
       
       const escrow = 
             new ethers.Contract(config[network.chainId].escrow.address, Escrow, provider)
